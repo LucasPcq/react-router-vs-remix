68395c2bf4401bce5f1935fef1e96a5a5e4c9e3e
LucasPcq <picquelucas17@gmail.com>
2024-05-14 16:01:26 +0200

    chore(app): setup remix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { vitePlugin as remix } from "@remix-run/dev";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    remix({
+      ssr: false,
+    }),
+  ],
+});
